43d1645ff9828e154dd2a66c47079239e2c79752
ridasiddiqui123 <ridasiddiqui123@gmail.com>
2024-06-25 19:10:03 -0700

    Changed App.jsx to the one Robert wrote

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled, { ThemeProvider } from "styled-components"
 import { lightTheme } from "./utils/Themes"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -35,39 +34,3 @@ function App(){
   </ThemeProvider>
 }
 export default App
-=======
-import React from 'react'
-import styled, { ThemeProvider } from "styled-components"
-import {lightTheme} from "./utils/Themes"
-import {BrowserRouter, Route, Routes } from "react-router-dom"
-import ProductDetails from './ProductDetails'
-
-const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    background: ${({ theme}) => theme.bg };
-    color: ${({theme}) => theme.text_primary};
-    overflow-x: hidden;
-    overflow-y: hidden;
-    transition: all 2.0s ease;
-`;
-
-function App() {
-    return (
-        <ThemeProvider theme={lightTheme}>
-            <BrowserRouter>
-                <Container>
-                    <Routes>
-                        <Route path="/" exact element={<ProductDetails />} />
-                    </Routes>
-                </Container>
-            </BrowserRouter>
-        </ThemeProvider>
-    
-    )
-}
-
-export default App;
->>>>>>> rida
