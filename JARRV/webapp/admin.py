983c98b1b8d959118134d348b329be8590103b53
vaibhavi8 <vaibjhawar@gmail.com>
2024-06-27 23:48:10 -0700

    db changes in /admin of backend

--- JARRV/webapp/admin.py
+++ JARRV/webapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Item, Similar_Item
+from .models import Item, Similar_Item, User, PurchaseHistory
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Similar_Item)
+admin.site.register(User)
+admin.site.register(PurchaseHistory)
