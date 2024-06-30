97ea9cf1d262378e3388f26a801f9039356211de
ridasiddiqui123 <ridasiddiqui123@gmail.com>
2024-06-30 00:27:58 -0700

    Deleted unnecessary folders and code in ProductCard.jsx

--- frontend/src/components/cards/ProductCard.jsx
+++ frontend/src/components/cards/ProductCard.jsx
@@ -100,7 +100,6 @@ const StyledRating = muiStyled(Rating)({
   },
 });
 
-
 const Details = styled.div`
 import { useDispatch } from "react-redux";
 import { openSnackbar } from "../../redux/reducers/snackbarSlice";
@@ -145,24 +144,7 @@ const Percent = styled.div`
 
 
 
-
-const EmptyWaterDropIcon = (props) => (
-  <svg fill="None" stroke="grey" strokeWidth="10" opacity="1" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-     width="15px" height="15px" viewBox="0 0 264.564 264.564"
-     xml:space="preserve">
-  <g>
-    <g>
-      <path d="M132.281,264.564c51.24,0,92.931-41.681,92.931-92.918c0-50.18-87.094-164.069-90.803-168.891L132.281,0l-2.128,2.773
-        c-3.704,4.813-90.802,118.71-90.802,168.882C39.352,222.883,81.042,264.564,132.281,264.564z"/>
-    </g>
-  </g>
-  </svg>
-  
-);
-
-
 const ProductCard = ({ product }) => {
-
 
   //const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -305,9 +287,3 @@ const ProductCard = ({ product }) => {
 
 export default ProductCard;
 
-//`/shop/${product._id}`
-//product?.price?.off  
-
-/*<Rating value={3.5} sx={{ fontSize: "14px" }} />*/
-
-/*icon={<WaterDropIcon style={{fontSize: 'inherit'}}  />}*/
