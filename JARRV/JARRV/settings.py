d523701e446f6316cc2c75878f3f633a7caf413a
vaibhavi8 <vaibjhawar@gmail.com>
2024-07-05 23:42:12 -0700

    adding local host to allowed hosts

--- JARRV/JARRV/settings.py
+++ JARRV/JARRV/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-+6ow)pac-3(1s&37e6#cyz_e*so9(&#i=$#ul33-ledlcr22tj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jarrv.onrender.com']
+ALLOWED_HOSTS = ['jarrv.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
