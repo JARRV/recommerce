757d0aa888c9b12fac1538c8ed312be4f72b43d5
vaibhavi8 <vaibjhawar@gmail.com>
2024-07-06 00:43:23 -0700

    forgot to add the settings file

--- JARRV/JARRV/settings.py
+++ JARRV/JARRV/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "JARRV.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "frontend/dist")],
+        "DIRS": [os.path.join(BASE_DIR, "frontend", "dist")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,12 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "JARRV",  "staticfiles")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'JARRV', 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "JARRV", "static"),
-    os.path.join(BASE_DIR,  "frontend/dist/static"),
+  # Check this path
+    os.path.join(BASE_DIR, 'frontend', 'dist', 'static'),
 ]
 
 
