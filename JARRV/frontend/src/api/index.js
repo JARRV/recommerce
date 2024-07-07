b3b6d17cc6f98fcf80722db815af91211d873822
vaibhavi8 <vaibjhawar@gmail.com>
2024-07-06 18:55:15 -0700

    making url an enviornment variable for correct usage

--- JARRV/frontend/src/api/index.js
+++ JARRV/frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "jarrv.onrender.com" || "http://localhost:8000"
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:8000"
 })
 
 
